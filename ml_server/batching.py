6d1a280e64eb130668d5e58b3887635e9746bb15
Akash Sonowal <work.akashsonowal@gmail.com>
2024-03-13 07:59:12 +0530

    Update batching.py

--- ml_server/batching.py
+++ ml_server/batching.py
@@ -1,23 +1,30 @@
-import asyncio
-import uuid
+import logging
 import time
+import asyncio
 
-from typing import Callable
-from pydantic import BaseModel
+from typing import Any, Callable, List
+
+logger = logging.getLogger(__name__)
 
 class BatchQueue:
     def __init__(self, max_batch_size: int, batch_wait_timeout_s: int, handle_batch_func: Callable):
         self.queue = asyncio.Queue()
         self.max_batch_size = max_batch_size
         self.batch_wait_timeout_s = batch_wait_timeout_s
-        self.requests_available_event = asyncio.Event()
 
-        self.func = handle_batch_func
-        self._responses = {}
-         
+        self._loop = asyncio.get_event_loop()
+        self._requests_available_event = asyncio.Event()
+
+        self._handle_batch_task = None
+
+        if handle_batch_func is not None:
+            self._handle_batch_task = self._loop.create_task(
+                self._process_batches(handle_batch_func)
+            )
+        
     def put(self, request):
         self.queue.put_nowait(request)
-        self.requests_available_event.set()
+        self._requests_available_event.set()
     
     async def consume(self):
         batch = []
@@ -33,7 +40,7 @@ class BatchQueue:
             )
             try:
                 await asyncio.wait_for(
-                    self.requests_available_event.wait(), remaining_batch_time_s
+                    self._requests_available_event.wait(), remaining_batch_time_s
                 )
             except asyncio.TimeoutError:
                 pass
@@ -42,28 +49,38 @@ class BatchQueue:
                 batch.append(self.queue.get_nowait())
 
             if self.queue.empty():
-                self.requests_available_event.clear()
+                self._requests_available_event.clear()
 
             if (
                 time.time() - batch_start_time >= batch_wait_timeout_s
                 or len(batch) >= max_batch_size
             ):
                 break
-        
         return batch
     
-    async def process_request(self, data: BaseModel):
-        request_id = uuid.uuid4().hex
-        self.put((request_id, data))
-        while True:
-            if request_id in self._responses:
-                result = self._responses.pop(request_id)
-                return result
-            await asyncio.sleep(0.001)
+    async def process_request(self, item):
+        future = self._loop.create_future()
+        self.put((item, future))
+        return await future
     
-    async def process_batches(self):
+    async def _process_batches(self, func):
+        while not self._loop.is_closed():
+            await self._process_batch(func)
+
+    async def _process_batch(self, func: Callable) -> None:
         batch = await self.consume()
         assert len(batch) > 0
-        data = self.func(batch)
-        for item in data:
-            self._responses[item["uid"]] = item
+
+        items, futures = zip(*batch)
+
+        try:
+            results = func(items)
+
+            for result, future in zip(results, futures):
+                if not future.done():
+                    future.set_result(result)
+        
+        except Exception as e:
+            for future in futures:
+                if not future.done():
+                    future.set_exception(e)
